b92cc8a3e89167eee8beb62859d1c3a8cfebf1bf
Emmanuel Ihemegbulam <codeBlock@iceony.com>
2019-04-03 23:11:46 +0100

    [ feature #164993189 ] create account delete endpoint
    - Create method for account delete in account controller class file
    - Create middleware for validating delete request param data
    - Create route for account delete

--- server/middlewares/validate.js
+++ server/middlewares/validate.js
@@ -6,9 +6,16 @@ const validate = (req, res, next) => {
   };
   const validationError = validationResult(req).formatWith(errorFormatter);
   if (!validationError.isEmpty()) {
+    const errorMsg = validationError.array()[0];
+    if (errorMsg.search('not exist') !== -1 || errorMsg.search('not found') !== -1) {
+      return res.status(404).json({
+        status: 404,
+        error: errorMsg,
+      });
+    }
     return res.status(400).json({
       status: 400,
-      error: validationError.array()[0],
+      error: errorMsg,
     });
   }
   return next();
--- server/routes/accountRoute.js
+++ server/routes/accountRoute.js
@@ -6,12 +6,13 @@ import accountValidator from '../middlewares/accountValidator';
 
 const router = express.Router();
 
-const { createAccount, updateAccountStatus } = AccountController;
+const { createAccount, updateAccountStatus, deleteAccount } = AccountController;
 const {
   accountFieldsValidator, accountStatusValidator, accountParamValidator, duplicateValidator,
 } = accountValidator;
 
 router.post('/', accountFieldsValidator, accountStatusValidator, duplicateValidator, validate, createAccount);
 router.patch('/:accountNumber', accountParamValidator, accountStatusValidator, validate, updateAccountStatus);
+router.delete('/:accountNumber', accountParamValidator, validate, deleteAccount);
 
 export default router;
