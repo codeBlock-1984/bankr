7daf5c7bbd5af45d8a4aeafcc2e0a5ef7a472cd7
Emmanuel Ihemegbulam <codeBlock@iceony.com>
2019-04-09 00:33:10 +0100

    [ chore #165187165 ] write users edpoints tests
    - Write tests for single user get request
    - Write tests for all users get request

---
+++ server/tests/userTest.js
@@ -0,0 +1,69 @@
+/* eslint-env mocha */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index';
+
+chai.use(chaiHttp);
+chai.should();
+
+const testUserData = {
+  firstName: 'Maureen',
+  lastName: 'George',
+  email: 'maumau@yahoo.com',
+  password: 'maureensecurepassword',
+  type: 'staff',
+  isAdmin: true,
+};
+let testUserId;
+const noId = 133;
+/* eslint-disable no-unused-vars */
+
+describe('Users Endpoints', () => {
+  describe('GET /users/:userId', () => {
+    before('seed users dummy data', async () => {
+      const response = await chai.request(app).post('/api/v1/auth/signup').send(testUserData);
+      testUserId = response.body.data.id;
+    });
+    it('should get a single user with the specified id', (done) => {
+      chai.request(app).get(`/api/v1/users/${testUserId}`).end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('status').eql(200);
+        res.body.should.have.property('data');
+        res.body.data.should.be.an('object');
+        res.body.data.should.have.property('firstName');
+        res.body.data.firstName.should.be.a('string');
+        res.body.data.should.have.property('lastName');
+        res.body.data.lastName.should.be.a('string');
+        res.body.data.should.have.property('email');
+        res.body.data.email.should.be.a('string');
+        res.body.data.should.have.property('password');
+        res.body.data.password.should.be.a('string');
+        res.body.data.should.have.property('isAdmin');
+        res.body.data.isAdmin.should.be.a('boolean');
+        res.body.data.should.have.property('type');
+        res.body.data.type.should.be.a('string');
+        res.body.data.should.have.property('id');
+        res.body.data.id.should.be.an('number');
+        done();
+      });
+    });
+    it('should return a 404 error if id is not found', (done) => {
+      chai.request(app).get(`/api/v1/users/${noId}`).end((err, res) => {
+        res.should.have.status(404);
+        res.body.should.have.property('status').eql(404);
+        res.body.should.have.property('error').eql('User with specified id not found!');
+        done();
+      });
+    });
+  });
+  describe('GET /users', () => {
+    it('should get all users', (done) => {
+      chai.request(app).get('/api/v1/users').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('status').eql(200);
+        res.body.should.have.property('data');
+        done();
+      });
+    });
+  });
+});
