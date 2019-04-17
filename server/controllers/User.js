c148303552c7e7ca65cce1abb9536d13b3cb133d
Emmanuel Ihemegbulam <codeBlock@iceony.com>
2019-04-17 23:29:57 +0100

    feature(db): create get all users method
    - Refactor get all users method to fetch users data from the database
    
    [Finishes #165429179]

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -15,10 +15,29 @@ class UserController {
   }
 
   static async getAllUsers(req, res) {
-    return res.status(200).json({
-      status: 200,
-      data: allUsers,
-    });
+    const client = await pool.connect();
+    try {
+      const getAllUsersQuery = `SELECT * FROM users ORDER BY id ASC`;
+      const { rows } = await client.query(getAllUsersQuery);
+      if (!rows[0]) {
+        return res.status(404).json({
+          status: 404,
+          error: 'No user records found!',
+        });
+      }
+      const allUsers = rows;
+      return res.status(200).json({
+        status: 200,
+        data: allUsers,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: 500,
+        error: 'Internal server error!',
+      });
+    } finally {
+      await client.release();
+    }
   }
 }
 
