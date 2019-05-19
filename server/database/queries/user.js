25ecd2f2576b40b34c3a5fc011d149d5e493925a
Emmanuel Ihemegbulam <codeBlock@iceony.com>
2019-05-19 14:21:02 +0100

    bug(crud): resolve delete user action not stored in db
    - Rebuild 'actions' table to fix contraint violation errors
    - Refactor deleteUser method to update 'actions' table on every delete
    - Refactor seed.js to accommadate new 'actions' table columns
    - Modify action.js query to accommadate new 'actions' table columns
    - Add getUserEmail query to user queries
    
    [Finishes #166107114]

--- server/database/queries/user.js
+++ server/database/queries/user.js
@@ -2,6 +2,11 @@ const getUser = `SELECT id, firstName AS firstName, lastName AS lastName, email,
                  type, isAdmin as isAdmin, createdOn AS createdOn FROM users
                  WHERE id = $1`;
 
+const getUserEmail = `SELECT id, firstName AS firstName, lastName AS lastName,
+                      email, type, isAdmin as isAdmin, createdOn AS createdOn
+                      FROM users
+                      WHERE email = $1`;
+
 const getAllUsers = `SELECT id, firstName AS firstName, lastName AS lastName,
                      email, type, isAdmin as isAdmin, createdOn AS createdOn
                      FROM users ORDER BY id ASC`;
@@ -11,6 +16,7 @@ const deleteUser = `DELETE FROM users WHERE email = $1
 
 export default {
   getUser,
+  getUserEmail,
   getAllUsers,
   deleteUser,
 };
